741213d52237e2372dab02587da2aa242897b7b5
Benjamin Wong <benjamin02615@gmail.com>
2025-03-21 13:37:23 -0700

    Merged DF

--- main/Data.py
+++ main/Data.py
@@ -4,6 +4,7 @@
 import requests
 import certifi
 import pandas as pd
+from io import StringIO
 
 # Initialize WebDriver
 
@@ -84,11 +85,36 @@ import pandas as pd
 
 # Now parse the HTML with pandas
 
+def remove_parentheses(text):
+    for i in range(len(text)):
+        if text[i] == "(":
+            text = text[:i - 1].strip()
+            break
+    return text
+
+def remove_comma(text):
+    for i in range(len(text)):
+        if text[i] == ",":
+            text = text[:i].strip()
+            break
+    return text
+
+def remove_metropolitan(text):
+    for i in range(len(text) - 4):
+        if (text[i].lower() == "m" and
+            text[i + 1] == "e" and
+            text[i + 2] == "t" and
+            text[i + 3] == "r" and
+            text[i + 4] == 'o'):
+                text = text[:i].strip()
+                break
+    return text
+
 def get_table(url, i):
     # Fetch the page content using requests with certificate verification
     response = requests.get(url, verify=certifi.where())
     html_content = response.text
-    tables = pd.read_html(html_content)
+    tables = pd.read_html(StringIO(html_content))
     df = tables[i]
     return df
 
@@ -110,13 +136,27 @@ def main():
     selected_df_pop = df_pop[[('City[a]', 'City[a]'),
                    ('City proper[c]', 'Population'),
                    ('City proper[c]', 'Density (/km2)')]]
+    renamed_df_pop = selected_df_pop.rename(columns = {'City[a]' : 'City'})
+    renamed_df_pop.columns = [' '.join(col).strip() if isinstance(col, tuple) else col for col in
+                              renamed_df_pop.columns]
+    renamed_df_pop = renamed_df_pop.rename(columns={
+        "City City": "City",
+        "City proper[c] Population": "Population",
+        "City proper[c] Density (/km2)" : "Density (/km2)"
+    })
+
 
     df_gdp = get_table("https://en.wikipedia.org/wiki/List_of_cities_by_GDP", 2)
     turn_to_csv(df_gdp, "cities_and_gdp.csv")
     selected_df_gdp = df_gdp[['City proper/metropolitan area', 'Official est. GDP up to date (billion US$)']]
     renamed_df_gdp = selected_df_gdp.rename(columns = {'City proper/metropolitan area' : 'City',
                                                        'Official est. GDP up to date (billion US$)' : 'GDP (billion US$)'})
-    print(renamed_df_gdp.head())
+    renamed_df_gdp['GDP (billion US$)'] = renamed_df_gdp['GDP (billion US$)'].apply(remove_parentheses)
+    renamed_df_gdp['City'] = renamed_df_gdp['City'].apply(remove_metropolitan)
+    renamed_df_gdp['City'] = renamed_df_gdp['City'].apply(remove_comma)
+
+    merged_df = pd.merge(renamed_df_pop, renamed_df_gdp, on = 'City', how = 'inner')
+    print(merged_df.head())
 
 
 
